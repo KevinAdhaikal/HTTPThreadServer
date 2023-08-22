64f8d336124f7ee8c74d9a246dab44186341d0bf
Kevin Adhaikal <109958650+KevinAdhaikal@users.noreply.github.com>
2023-08-22 17:31:33 +0700

    menambahkan function

--- httplibrary.h
+++ httplibrary.h
@@ -43,6 +43,8 @@ void http_buffer_free(http_buffer* b);
 void http_send_status(http_event* e, int status, const char *val);
 void http_send_header(http_event* e, const char *name, const char *val);
 void http_write(http_event* e, const char *data, int len);
+void http_get_header(http_event* e, const char* header_name, char* dest, size_t dest_len);
+void http_get_cookie(http_event* e, const char *cookie_name, char *dest, size_t dest_len);
 int http_send_file(http_event* e, const char* filename);
 
 #endif // HTTPLIBRARY_H
