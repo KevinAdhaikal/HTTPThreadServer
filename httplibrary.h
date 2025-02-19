0b2a6f55b666bebc6cf0b3ebc9f94653caa1788d
Kevin Adhaikal <109958650+KevinAdhaikal@users.noreply.github.com>
2025-02-19 10:24:46 +0700

    Update httplibrary.h

--- httplibrary.h
+++ httplibrary.h
@@ -3,14 +3,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+#include <ctype.h>
+#include <sys/stat.h>
 
 #ifdef _WIN32
 #include <winsock2.h>
 #include <windows.h>
 #include <ws2tcpip.h>
 #include <mswsock.h>
-
-#define http_close_socket(socket) closesocket(socket)
 #else
 #include <unistd.h>
 #include <pthread.h>
@@ -19,19 +21,24 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/select.h>
 #include <sys/sendfile.h>
+#include <sys/epoll.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <fcntl.h>
 #include <ctype.h>
+#include <errno.h>
 
 #define SOCKET int
+#define SD_RECEIVE SHUT_RD
+#define SD_SEND SHUT_WR
 #define SD_BOTH SHUT_RDWR
-#define http_close_socket(socket) close(socket)
 #endif
 
-#include "string_lib.h"
+#include "buffer_lib.h"
+
+typedef size_t cmp_stream_stat;
+#define HTTP_TEMPORARY_MAX_BUFFER 8192
 
 typedef struct {
     char* val;
@@ -40,28 +47,51 @@ typedef struct {
 
 typedef struct {
     SOCKET socket;
-
+    
     // Parsing result
     http_string method;
     http_string path;
     char* query_pointer;
-    size_t query_list_length;
     http_string version;
     char* headers_pointer;
-    size_t headers_length;
     char* cookie_pointer;
     http_string body;
+
+    struct sockaddr_in addr;
 } http_client;
 
 typedef void (*http_callback)(http_client*);
 
-char* http_get_query(http_client* client, const char* key); // HTTP Get Query
-char* http_get_header(http_client* client, const char* key); // HTTP Get Header
-char* http_get_cookie(http_client* client, const char* key); // HTTP Get Cookie
-char http_write(SOCKET s, const char* data, unsigned long long int size); // HTTP Write
-char http_send_file(SOCKET s, const char* name_file, char manual_code); // HTTP Send File
+typedef struct {
+    #ifndef _WIN32
+    pthread_t thread_id;
+    #endif
+    http_callback callback;
+    buffer req_raw_data;
+    http_client* client;
+    cmp_stream_stat status;
+    size_t body_len_remaining;
+    char no_need_check_request;
+} http_room;
 
-SOCKET http_init_socket(const char* ip, unsigned short port); // initilaize socket
-void http_start(SOCKET s_socket, http_callback callback); // Start HTTP
+typedef struct {
+    char still_on; // memberi tahu bahwa server ini masih on atau off
+    size_t max_sockets; // buat nge cek max threadsd
+    SOCKET server_socket; // server socket nya
+    http_room* thread_rooms; // isi isi room untuk http.
+} http;
+
+#define http_get_query(query_pointer, key) __http_get_query(query_pointer, key, sizeof(key) - 1) // HTTP Get Query
+#define http_get_header(headers_pointer, key) __http_get_header(headers_pointer, key, sizeof(key) - 1) // HTTP Get Header
+char* __http_get_query(char* query_pointer, const char* key, size_t key_len); // HTTP Get Query
+char* __http_get_header(char* headers_pointer, const char* key, size_t key_len); // HTP Get Header
+char* http_get_cookie(http_client* client, const char* key); // HTTP Get Cookie
+char http_write(http_client* client, const char* data, size_t size); // HTTP Write
+#define http_write_string(client, str) http_write(client, str, sizeof(str) - 1) // HTTP Write String
+char http_send_file(http_client* client, const char* name_file, char manual_code, char use_cache); // HTTP Send File
+
+http* http_init_socket(const char* ip, unsigned short port, size_t max_threads, http_callback callback); // initilaize socket
+void http_start(http* http); // Start HTTP
+void http_stop(http* http); // Stop HTTP
 
 #endif // HTTP_HEADER_INCLUDED
