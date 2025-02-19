f5ebf0afc7cc4970846e2bfa361c26035411fddf
Kevin Adhaikal <109958650+KevinAdhaikal@users.noreply.github.com>
2025-02-19 10:25:49 +0700

    Add files via upload

---
+++ buffer_lib.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#include "buffer_lib.h"
+
+#define DEFAULT_INIT_SIZE 512
+
+#define ALLOC_CUR_REALLOC(buf, temp_size) \
+if (buf->alloc_cur <= (buf->len + temp_size)) { \
+    while (buf->alloc_cur <= (buf->len + temp_size)) { \
+        buf->alloc_cur += DEFAULT_INIT_SIZE; \
+    } \
+    buf->val = realloc(buf->val, buf->alloc_cur); \
+}
+
+void buffer_init(buffer* buf) {
+    memset(buf, 0, sizeof(buffer));
+    buf->val = malloc(DEFAULT_INIT_SIZE);
+    buf->alloc_cur = DEFAULT_INIT_SIZE;
+    *buf->val = 0;
+}
+
+void buffer_string_append(buffer* buf, const char* val) {
+    size_t buf_len = strlen(val);
+    ALLOC_CUR_REALLOC(buf, buf_len);
+
+    memcpy(buf->val + buf->len, val, buf_len);
+    buf->len += buf_len;
+}
+
+void buffer_append_n(buffer* buf, const char* val, size_t len) {
+    ALLOC_CUR_REALLOC(buf, len);
+
+    memcpy(buf->val + buf->len, val, len);
+    buf->len += len;
+}
+
+void buffer_append_char(buffer* buf, const char val) {
+    ALLOC_CUR_REALLOC(buf, 1);
+    buf->val[buf->len++] = val;
+}
+
+void buffer_begin(buffer* buf) {
+    *buf->val = 0;
+    buf->len = 0;
+}
+
+void buffer_begin_reinit(buffer* buf) {
+    if (buf->val) {
+        free(buf->val);
+        buf->val = malloc(DEFAULT_INIT_SIZE);
+        *buf->val = 0;
+        buf->len = 0;
+        buf->alloc_cur = DEFAULT_INIT_SIZE;
+    }
+    
+}
+
+void buffer_finalize(buffer* buf) {
+    if (buf->val) free(buf->val);
+    buf->val = NULL;
+    buf->len = 0;
+    buf->alloc_cur = 0;
+}
---
+++ buffer_lib.h
@@ -0,0 +1,23 @@
+#ifndef STRING_LIB_HEADER
+#define STRING_LIB_HEADER
+
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h> 
+
+
+typedef struct {
+    char* val;
+    size_t len;
+    size_t alloc_cur;
+} buffer;
+
+void buffer_init(buffer* str);
+void buffer_string_append(buffer* str, const char* val);
+void buffer_append_n(buffer* str, const char* val, size_t len);
+void buffer_append_char(buffer* str, const char val);
+void buffer_begin(buffer* str);
+void buffer_begin_reinit(buffer* str);
+void buffer_finalize(buffer* str);
+
+#endif //STRING_LIB_HEADER
