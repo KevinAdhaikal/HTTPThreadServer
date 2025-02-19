686037dc0fc11372dfb6073d9561c1fe47ca3024
Kevin Adhaikal <109958650+KevinAdhaikal@users.noreply.github.com>
2025-02-19 10:24:21 +0700

    Update example.c

--- example.c
+++ example.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
+#include <string.h>
+
 #include "httplibrary.h"
 
 void http_handler(http_client* client) {
-    send(client->socket, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nHello World", 55, 0);
+    http_write_string(client, "HTTP/1.1 200 OK\r\n\r\nHello World");
 }
 
 int main() {
-    SOCKET s_socket = http_init_socket("0.0.0.0", 8080);
-    http_start(s_socket, http_handler);
-
-    http_close_socket(s_socket);
-
+    http_start(http_init_socket("127.0.0.1", 8080, 1024, http_handler));
     return 0;
 }
